3e777fe4c7ddcdc5083da5d0dabc78a7108e0aaf
Kevin Dai <daikevin19@yahoo.com>
2019-05-02 16:26:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  new_sentence = sentence.split(" ") ##split by space
+  new_sentence = sentence.split(" ")
   answer = []
   new_sentence.collect do |word|
     answer.push(word.reverse)
